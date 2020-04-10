12e16f74001fd3da2dfb06a28b8f6ce0641cb92b
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2020-04-10 19:48:48 +0200

    WIP

--- src/Contracts/TransactionInterface.php
+++ src/Contracts/TransactionInterface.php
@@ -13,4 +13,10 @@ interface TransactionInterface
     const PHASE_INITIALIZATION = 'Initialisation';
     const PHASE_RECEIPT = 'Receipt';
     const PHASE_TRANSFER = 'Transfer';
+
+    //The value of the acknowledgement is 0 (“positive acknowledgement”) if download and processing of the order data was successful
+    const CODE_RECEIPT_POSITIVE = 0;
+
+    // Otherwise the value of the acknowledgement is 1 (“negative acknowledgement”).
+    const CODE_RECEIPT_NEGATIVE = 1;
 }
--- src/EbicsClient.php
+++ src/EbicsClient.php
@@ -4,6 +4,7 @@ namespace AndrewSvirin\Ebics;
 
 use AndrewSvirin\Ebics\Contracts\EbicsClientInterface;
 use AndrewSvirin\Ebics\Contracts\EbicsResponseExceptionInterface;
+use AndrewSvirin\Ebics\Exceptions\NoDownloadDataAvailableException;
 use AndrewSvirin\Ebics\Factories\CertificateFactory;
 use AndrewSvirin\Ebics\Factories\EbicsExceptionFactory;
 use AndrewSvirin\Ebics\Factories\TransactionFactory;
@@ -14,6 +15,7 @@ use AndrewSvirin\Ebics\Models\Bank;
 use AndrewSvirin\Ebics\Models\KeyRing;
 use AndrewSvirin\Ebics\Models\Request;
 use AndrewSvirin\Ebics\Models\Response;
+use AndrewSvirin\Ebics\Models\Transaction;
 use AndrewSvirin\Ebics\Models\User;
 use AndrewSvirin\Ebics\Services\CryptService;
 use DateTime;
@@ -87,15 +89,17 @@ final class EbicsClient implements EbicsClientInterface
     public function post(Request $request): ResponseInterface
     {
         $body = $request->getContent();
+        //dump($body);
         $httpClient = HttpClient::create();
         $response = $httpClient->request('POST', $this->bank->getUrl(), [
-         'headers' => [
-            'Content-Type' => 'text/xml; charset=ISO-8859-1',
-         ],
-         'body' => $body,
-         'verify_peer' => false,
-         'verify_host' => false,
-      ]);
+            'headers' => [
+                'Content-Type' => 'text/xml; charset=ISO-8859-1',
+            ],
+            'body' => $body,
+            'verify_peer' => false,
+            'verify_host' => false,
+        ]);
+        //dump($response->getContent());
 
         return $response;
     }
@@ -285,6 +289,7 @@ final class EbicsClient implements EbicsClientInterface
         if (null === $dateTime) {
             $dateTime = DateTime::createFromFormat('U', time());
         }
+
         $request = $this->requestFactory->buildHTD($dateTime);
         $hostResponse = $this->post($request);
         $hostResponseContent = $hostResponse->getContent();
@@ -302,6 +307,107 @@ final class EbicsClient implements EbicsClientInterface
 
         return $response;
     }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+     * @throws Exceptions\EbicsException
+     * @throws Exceptions\EbicsResponseException
+     * @throws Exceptions\NoDownloadDataAvailableException
+     */
+    public function FDL(string $fileInfo, string $countryCode = 'FR', DateTime $dateTime = null, DateTime $startDateTime = null, DateTime $endDateTime = null): Response
+    {
+        if (null === $dateTime) {
+            $dateTime = DateTime::createFromFormat('U', time());
+        }
+
+        $request = $this->requestFactory->buildFDL($dateTime, $fileInfo, $countryCode, $startDateTime, $endDateTime);
+        $hostResponse = $this->post($request);
+        $hostResponseContent = $hostResponse->getContent();
+        /*
+                $hostResponseContent = <<<EOF
+        <?xml version="1.0" encoding="UTF-8"?>
+        <ebicsResponse xmlns="urn:org:ebics:H004" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" Version="H004" Revision="1" xsi:schemaLocation="urn:org:ebics:H004 ebics_response_H004.xsd">
+            <header authenticate="true">
+                <static>
+                    <TransactionID>520F251CBA36F4852B84BF8C75540B05</TransactionID>
+                    <NumSegments>1</NumSegments>
+                </static>
+                <mutable>
+                    <TransactionPhase>Initialisation</TransactionPhase>
+                    <SegmentNumber lastSegment="true">1</SegmentNumber>
+                    <ReturnCode>000000</ReturnCode>
+                    <ReportText>[EBICS_OK] OK</ReportText>
+                </mutable>
+            </header>
+            <AuthSignature>
+                <ds:SignedInfo>
+                    <ds:CanonicalizationMethod Algorithm="http://www.w3.org/TR/2001/REC-xml-c14n-20010315"/>
+                    <ds:SignatureMethod Algorithm="http://www.w3.org/2001/04/xmldsig-more#rsa-sha256"/>
+                    <ds:Reference URI="#xpointer(//*[@authenticate='true'])">
+                        <ds:Transforms>
+                            <ds:Transform Algorithm="http://www.w3.org/TR/2001/REC-xml-c14n-20010315"/>
+                        </ds:Transforms>
+                        <ds:DigestMethod Algorithm="http://www.w3.org/2001/04/xmlenc#sha256"/>
+                        <ds:DigestValue>pQXSMDUv7ku8htldih4VAAdxlE4aB5Vbu+ihTH86XjY=</ds:DigestValue>
+                    </ds:Reference>
+                </ds:SignedInfo>
+                <ds:SignatureValue>j2x3sw9ZY5aj9PQnViQZWh6x/zpVzoRT3YZ7YJuQmgtqtLxcLWFDS6aj3+Pu78LEQzwcjne7wodePxqLy4XpbOH1hsfk4/hoPa03f7vGnyHLJRNCmk12GiKKvp71oUWPy32/0nAcUReZKewBD1cVZ4NzlRLTdhIRfBTz+fb7uVrwvFY5d70yD44AqoSMmx2N88gC0UCDdrW1oAexsuIxM1JJ6NUDqptZzCL4izpKvfvM5IO+/UaaQrqrf9aTrabmtek/hmnelM0H2+3NyhqsMt3M8uQt18JRffvVlKCyTOE1YJx1JRRhzLMSbR2iK8iNLGReFbtzFyoX5VxObt8yGQ==</ds:SignatureValue>
+            </AuthSignature>
+            <body>
+                <DataTransfer>
+                    <DataEncryptionInfo authenticate="true">
+                        <EncryptionPubKeyDigest Version="E002" Algorithm="http://www.w3.org/2001/04/xmlenc#sha256">Mh1mHtEtQ3R6ipob/yXAxZDNMnN1VC35YZR6ZdZtO2k=</EncryptionPubKeyDigest>
+                        <TransactionKey>B2u+3wy/lEhMaOpn6u9jtb1kS1WZDwLTlgfdx+2+iF3oO/lY+qn08iSGbsGCiwo3QsPr8xIiO4ywRUWWHbh99GhGGzUgFeKVo2NzyQ4b1tySGnETbsbMbcoiHM7TeawYsMasq6cBn66PzpPl/BOANfgQSQfNVxcI1Cqhxfh7Hnv4+SviZWVm+kniursZpS2yeq02DrMxqo9JIUu20slGDCl5lwX7W+hvWPn7VdQ1/WZLtR25T330KtEemERhe/jsH2KdDYWQuXISRm1nSWzE96Nl7KGS1/V4GTRgwhh7eP19jNO6EiK+WgA/Gmijhi9bhQTwBvkFWFoyFh0h7kIndQ==</TransactionKey>
+                    </DataEncryptionInfo>
+                    <OrderData>4ZkdKoGH3AKAg2R+x9r3JBP4zhm0of3NPPgpcg3HnLLGle442oMpetUkduGfK5UkMEXe9DFUH/A1Jc4cvt8nJPA7oUnn7E0erSB2Pmb68cg=</OrderData>
+                </DataTransfer>
+                <ReturnCode authenticate="true">000000</ReturnCode>
+                <TimestampBankParameter authenticate="true">2016-02-17T12:18:48.548Z</TimestampBankParameter>
+            </body>
+        </ebicsResponse>
+        EOF;
+        */
+        $response = new Response();
+        $response->loadXML($hostResponseContent);
+
+        $this->checkH004ReturnCode($request, $response);
+
+        //EBICS_NO_DOWNLOAD_DATA_AVAILABLE
+        if ('090005' === $this->responseHandler->retrieveH004BodyOrHeaderReturnCode($response)) {
+            throw new NoDownloadDataAvailableException($this->responseHandler->retrieveH004ReportText($response));
+        }
+
+        $transaction = $this->responseHandler->retrieveTransaction($response);
+        $response->addTransaction($transaction);
+
+        // Prepare decrypted OrderData.
+        $orderDataEncrypted = $this->responseHandler->retrieveOrderData($response);
+        $orderDataContent = CryptService::decryptOrderDataContent($this->keyRing, $orderDataEncrypted);
+        $transaction->setPlainOrderData($orderDataContent);
+
+        return $response;
+    }
+
+    public function fileAcknowledgement(Transaction $transaction)
+    {
+        $request = $this->requestFactory->buildFileAcknowledgement($transaction);
+        $hostResponse = $this->post($request);
+        $hostResponseContent = $hostResponse->getContent();
+        $response = new Response();
+        $response->loadXML($hostResponseContent);
+
+        try {
+            $this->checkH004ReturnCode($request, $response);
+        } catch (EbicsResponseExceptionInterface $exception) {
+            //EBICS_DOWNLOAD_POSTPROCESS_DONE, means download is OK
+            if ('011000' !== $exception->getResponseCode()) {
+                throw $exception;
+            }
         }
 
         return $response;
