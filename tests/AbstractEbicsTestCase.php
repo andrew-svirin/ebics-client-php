dca651b327b366a6d2b2d4be78be1fc2d150ec5f
Andriy Svirin <andriy.svirin@ukrinsoft.com>
2020-12-18 16:07:47 -0500

    Up unit tests.

--- tests/AbstractEbicsTestCase.php
+++ tests/AbstractEbicsTestCase.php
@@ -3,13 +3,11 @@
 namespace AndrewSvirin\Ebics\Tests;
 
 use AndrewSvirin\Ebics\EbicsClient;
-use AndrewSvirin\Ebics\Exceptions\EbicsException;
 use AndrewSvirin\Ebics\Models\Bank;
 use AndrewSvirin\Ebics\Models\Certificate;
 use AndrewSvirin\Ebics\Models\KeyRing;
 use AndrewSvirin\Ebics\Models\User;
 use AndrewSvirin\Ebics\Services\KeyRingManager;
-use AndrewSvirin\Ebics\Tests\Utils\EnvUtil;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -31,7 +29,7 @@ abstract class AbstractEbicsTestCase extends TestCase
     /**
      * @var int
      */
-    private $credentialsId = 3;
+    protected $credentialsId = 1;
 
     /**
      * @var EbicsClient
@@ -58,13 +56,9 @@ abstract class AbstractEbicsTestCase extends TestCase
      */
     protected $user;
 
-    /**
-     * @throws EbicsException
-     */
     protected function setupClient()
     {
-        $credentialsDataProvider = $this->credentialsDataProvider();
-        $credentials = $credentialsDataProvider[$this->credentialsId];
+        $credentials = $this->credentialsDataProvider($this->credentialsId);
         $keyRingRealPath = sprintf('%s/workspace/keyring_%d.json', $this->data, $this->credentialsId);
         $this->bank = new Bank($credentials['hostId'], $credentials['hostURL'], $credentials['hostIsCertified']);
         $this->user = new User($credentials['partnerId'], $credentials['userId']);
@@ -78,23 +72,23 @@ abstract class AbstractEbicsTestCase extends TestCase
         $keys = json_decode(file_get_contents($this->fixtures . '/keys.json'));
         $this->keyRing->setPassword('mysecret');
         $this->keyRing->setUserCertificateX(new Certificate(
-         $this->keyRing->getUserCertificateX()->getType(),
-         $this->keyRing->getUserCertificateX()->getPublicKey(),
-         $keys->X002,
-         $this->keyRing->getUserCertificateX()->getContent()
-      ));
+            $this->keyRing->getUserCertificateX()->getType(),
+            $this->keyRing->getUserCertificateX()->getPublicKey(),
+            $keys->X002,
+            $this->keyRing->getUserCertificateX()->getContent()
+        ));
         $this->keyRing->setUserCertificateE(new Certificate(
-         $this->keyRing->getUserCertificateE()->getType(),
-         $this->keyRing->getUserCertificateE()->getPublicKey(),
-         $keys->E002,
-         $this->keyRing->getUserCertificateX()->getContent()
-      ));
+            $this->keyRing->getUserCertificateE()->getType(),
+            $this->keyRing->getUserCertificateE()->getPublicKey(),
+            $keys->E002,
+            $this->keyRing->getUserCertificateX()->getContent()
+        ));
         $this->keyRing->setUserCertificateA(new Certificate(
-         $this->keyRing->getUserCertificateA()->getType(),
-         $this->keyRing->getUserCertificateA()->getPublicKey(),
-         $keys->A006,
-         $this->keyRing->getUserCertificateX()->getContent()
-      ));
+            $this->keyRing->getUserCertificateA()->getType(),
+            $this->keyRing->getUserCertificateA()->getPublicKey(),
+            $keys->A006,
+            $this->keyRing->getUserCertificateX()->getContent()
+        ));
     }
 
     /**
@@ -113,87 +107,22 @@ abstract class AbstractEbicsTestCase extends TestCase
 
     /**
      * Client credentials data provider.
-     */
-    public function credentialsDataProvider(): array
-    {
-        $secret = EnvUtil::getSecret();
-        $this->generateCredentialsDataProvider($secret);
-        $credentialsEnc = json_decode(file_get_contents($this->credentialsFilePath()), true);
-        $credentialsDec = [];
-        foreach ($credentialsEnc as $i => $credentials) {
-            $credentialsDec[$i] = [
-            'hostId' => $this->decrypt($credentials['hostId'], $secret),
-            'hostURL' => $this->decrypt($credentials['hostURL'], $secret),
-            'hostIsCertified' => (bool) $this->decrypt($credentials['hostIsCertified'], $secret),
-            'partnerId' => $this->decrypt($credentials['partnerId'], $secret),
-            'userId' => $this->decrypt($credentials['userId'], $secret),
-         ];
-        }
-
-        return $credentialsDec;
-    }
-
-    /**
-     * @return string
-     */
-    private function credentialsFilePath()
-    {
-        return $this->data . '/credentials.json';
-    }
-
-    /**
-     * Credentials data provider.
-     * Extract from env debugging variables and populate data.
-     */
-    private function generateCredentialsDataProvider(string $secret)
-    {
-        if (!($credentials1 = EnvUtil::getCredentials1()) || !($credentials2 = EnvUtil::getCredentials2()) || !($credentials3 = EnvUtil::getCredentials3())) {
-            return;
-        }
-        $credentialsEnc = [];
-        for ($i = 1; $i < 4; ++$i) {
-            switch ($i) {
-            case 1:
-               $credentials = $credentials1;
-               break;
-            case 2:
-               $credentials = $credentials2;
-               break;
-            case 3:
-               $credentials = $credentials3;
-               break;
-            default:
-               $credentials = null;
-         }
-            if (null === $credentials) {
-                continue;
-            }
-            $credentialsEnc[$i] = [
-            'hostId' => $this->encrypt($credentials['hostId'], $secret),
-            'hostURL' => $this->encrypt($credentials['hostURL'], $secret),
-            'hostIsCertified' => $this->encrypt($credentials['hostIsCertified'], $secret),
-            'partnerId' => $this->encrypt($credentials['partnerId'], $secret),
-            'userId' => $this->encrypt($credentials['userId'], $secret),
-         ];
-        }
-        file_put_contents($this->credentialsFilePath(), json_encode($credentialsEnc, \JSON_PRETTY_PRINT));
-    }
-
-    /**
-     * Encrypt text.
-     */
-    private function encrypt(string $text, string $secret): string
-    {
-        return base64_encode(openssl_encrypt($text, self::ENCRYPT_ALGO, $secret));
-    }
-
-    /**
-     * Decrypt text.
      *
-     * @return string
+     * @param int $credentialsId
+     *
+     * @return array
      */
-    private function decrypt(string $text, string $secret)
+    public function credentialsDataProvider($credentialsId): array
     {
-        return openssl_decrypt(base64_decode($text), self::ENCRYPT_ALGO, $secret);
+        $path = sprintf('%s/credentials/credentials_%d.json', $this->data, $credentialsId);
+        $credentialsEnc = json_decode(file_get_contents($path), true);
+
+        return [
+            'hostId' => $credentialsEnc['hostId'],
+            'hostURL' => $credentialsEnc['hostURL'],
+            'hostIsCertified' => (bool)$credentialsEnc['hostIsCertified'],
+            'partnerId' => $credentialsEnc['partnerId'],
+            'userId' => $credentialsEnc['userId'],
+        ];
     }
 }
