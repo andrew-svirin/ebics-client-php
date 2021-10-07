28a148fa552ee15df5195068035c4f52d5ea1335
MaisOui <festivjo@gmail.com>
2021-10-07 10:31:12 +0200

    Try to use calculateCertificateFingerprint instead of calculateKeyHash

--- src/Services/CryptService.php
+++ src/Services/CryptService.php
@@ -333,7 +333,7 @@ class CryptService
 
         $key = $this->calculateKey($exponent, $modulus);
 
-        return $this->calculateKeyHash($key, $algorithm, $rawOutput);
+        return $this->calculateCertificateFingerprint($key, $algorithm, $rawOutput);
     }
 
     /**
