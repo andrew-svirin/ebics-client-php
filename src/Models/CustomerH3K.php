633e01f6fd73535cc3d7287ba226abdee0eba627
Ronan Giron <ElGigi@users.noreply.github.com>
2022-07-20 13:34:37 +0200

    H3K implementation

---
+++ src/Models/CustomerH3K.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AndrewSvirin\Ebics\Models;
+
+/**
+ * Class CustomerH3K represents CustomerH3K model.
+ *
+ * @license http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @author Ronan GIRON <https://github.com/ElGigi>
+ */
+class CustomerH3K extends OrderData
+{
+
+}
